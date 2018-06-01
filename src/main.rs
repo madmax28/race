6161eaf1d0f45415a9d3f591c8dc001a64c298b6
Vzaa <Vzaa@users.noreply.github.com>
2018-06-01 11:34:14 +0300

    Address clippy lints

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let program = std::env::args().nth(1).unwrap_or_else(|| usage());
     let args: Vec<String> = std::env::args().skip(1).collect();
 
-    let child = race::fork_child(&program, args);
+    let child = race::fork_child(&program, &args);
     let mut race = Race::new(child);
     race.trace();
     race.dump_tree();
