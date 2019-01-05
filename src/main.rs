a44715c919bf8352ad8c75700ec2a7b4929523a5
Maximilian Fricke <mfricke2808@gmail.com>
2019-01-05 11:58:46 +0100

    Allow dumping tree to file

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ mod race;
 mod tui;
 mod util;
 
+use crate::tui::Client;
 use crate::tui::{term, tv};
+
+use std::fs;
+use std::io;
+use std::io::Write;
 
 type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
@@ -20,6 +25,23 @@ fn main() {
     let child = race::fork_child(&program, &program_args);
     let mut race = race::Race::new(child);
     race.trace();
+
+    if let Some(filename) = args.value_of("OUTFILE") {
+        match fs::File::create(filename) {
+            Ok(f) => {
+                let mut bw = io::BufWriter::new(f);
+                for l in tv::TreeView::new(race.tree()).gen_lines() {
+                    if let Err(e) = writeln!(bw, "{}", l) {
+                        eprintln!("Error dumping tree: {}", e);
+                        break;
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Error opening file {}: {}", filename, e);
+            }
+        }
+    }
 
     if args.is_present("TUI") {
         let tv = tv::TreeView::new(race.tree());
